a82f92fe18fd7f5d88cb3cd9ceb960e84a3c6fb3
AllBundy <securaplayer@o2.pl>
2016-05-20 18:33:19 +0200

    Dodano strukturę OperationResult, posiadająca logiczną wartość rezultatu operacji (np. powodzenia usunięcia bazy danych) oraz ewentualny wyjątek w przypadku braku powodzenia; Dodano implementację metod w klasach statycznych Database oraz Table Dodano klasę ColumnStructureCreator, która to będzie reprezentowała kolumnę w bazie danych.

---
+++ src/DB-Editor/DB-Handlers/OperationResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DB_Editor.DB_Handlers
+{
+    public struct OperationResult
+    {
+        private bool isSucceded_;
+        private Exception exception_;
+        public OperationResult(bool result, Exception exc)
+        {
+            isSucceded_ = result;
+            exception_ = exc;
+        }
+        public bool IsSucceded { get { return isSucceded_; }}
+        public Exception Exception { get { return exception_; }}
+    };
+}
