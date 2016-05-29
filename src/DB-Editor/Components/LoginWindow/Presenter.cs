ef56cc57a7c3efcf857abaed40ed3ef4c0f748dc
AllBundy <securaplayer@o2.pl>
2016-05-29 17:13:51 +0200

    Update Presenter.cs

--- src/DB-Editor/Components/LoginWindow/Presenter.cs
+++ src/DB-Editor/Components/LoginWindow/Presenter.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DB_Editor.DB_Handlers;
-using DB_Editor.DB_Connection;
 
 namespace DB_Editor.Components.LoginWindow
 {
@@ -18,10 +16,6 @@ namespace DB_Editor.Components.LoginWindow
         {
             view_ = view;
             model_ = new Model();
-            //DB_Connection.DBConnectionManager.Connect();
-            //List<string> ofo = Database.GetTablesFromDatabase("fuu");
-            //foreach (var i in ofo)
-               // Console.WriteLine(i);
         }
 
         public void Connect()
