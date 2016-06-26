37554388e9899beb9c482a1bd4388513a019a40b
tomik3690 <tomik031195@gmail.com>
2016-06-26 11:20:42 +0200

    Dołączono klasę DisplayError

---
+++ src/DB-Editor/Events/DisplayError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Editor.Events
+{
+    public static class DisplayError
+    {
+        public static event Action<string, string> DisplayErrorEvent;
+
+        public static void FireDisplayErrorEvent(string message, string title = "Error")
+        {
+            if (DisplayErrorEvent != null)
+            {
+                DisplayErrorEvent(message, title);
+            }
+        }
+    }
+}
